c05d90fc11d8ed0a4ce5b33db367d149dd7a2067
caiohds <caiohds2020@gmail.com>
2022-09-27 16:04:01 -0300

    Criando a conexão com o banco de dados

---
+++ FilmesAPI/FilmesAPI/data/FilmeContext.cs
@@ -0,0 +1,14 @@
+﻿using FilmesAPI.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.data
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> opt) : base(opt)
+        {
+
+        }
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
