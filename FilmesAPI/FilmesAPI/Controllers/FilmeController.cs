44ab2edfd77580e0326514ca82085626d66ea675
caiohds <caiohds2020@gmail.com>
2022-09-27 13:21:32 -0300

    Criação do controller 'FilmeController' e da classe 'Filme'

---
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,22 @@
+﻿using FilmesAPI.models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController] // declara que a classe é um controlador
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List <Filme> filmes = new List<Filme>();
+
+        /// <summary>
+        /// Adiciona um filme na lista de filmes.
+        /// </summary>
+        /// <param name="filme">Passa como um parâmetro um filme</param>
+        public void AdicionarFilme(Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
