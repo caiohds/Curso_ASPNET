1defd01a352d53a628eadb354d31dd9f2267181c
caiohds <caiohds2020@gmail.com>
2022-09-29 17:51:24 -0300

    Atualizando e removendo dados

--- FilmesAPI1/FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI1/FilmesAPI/Controllers/FilmeController.cs
@@ -52,5 +52,40 @@ namespace FilmesAPI.Controllers
             }
             return NotFound();
         }
+        /// <summary>
+        /// Atualiza os valores de um objeto do tipo filme
+        /// </summary>
+        /// <param name="id">id do filme que será atualizado</param>
+        /// <param name="filmenovo">valores que serão atualizados no filme</param>
+        /// <returns>retorna o status de no content</returns>
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaFilme(int id, [FromBody] Filme filmenovo)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme == null)
+            {
+                return NotFound();
+            }
+            filme.Titulo = filmenovo.Titulo;
+            filme.Genero = filmenovo.Genero;
+            filme.Diretor = filmenovo.Diretor;
+            filme.Duracao = filmenovo.Duracao;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeletarFilme(int id)
+        {
+            Filme filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme == null)
+            {
+                return NotFound();
+            }
+            _context.Filmes.Remove(filme);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
 }
