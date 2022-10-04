6da52af94245ddab5bd76da15e38cfd104f99272
caiohds <caiohds2020@gmail.com>
2022-10-03 23:37:43 -0300

    Criação da classe 'Cinema'

---
+++ FilmesAPI1/FilmesAPI/models/Cinema.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.models
+{
+    public class Cinema
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo de nome é obrigatório")]
+        public string Nome { get; set; }
+
+    }
+}
