97922f30aee580119f4b7158d34f8bd3a4786a71
caiohds <caiohds2020@gmail.com>
2022-10-04 14:44:49 -0300

    Criação da classe Gerente

---
+++ FilmesAPI1/FilmesAPI/models/Gerente.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.models
+{
+    public class Gerente
+    {
+        [Key]
+        [Required]
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
