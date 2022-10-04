3ec70d4b9d7f87f570f14ffbf0c31f0132f7b599
caiohds <caiohds2020@gmail.com>
2022-10-03 23:39:05 -0300

    Criação do Dbset do Cinema

--- FilmesAPI1/FilmesAPI/data/FilmeContext.cs
+++ FilmesAPI1/FilmesAPI/data/FilmeContext.cs
@@ -1,14 +1,17 @@
 ﻿using FilmesAPI.models;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace FilmesAPI.data
 {
     public class FilmeContext : DbContext
     {
-        public FilmeContext(DbContextOptions<FilmeContext> opt) : base(opt)
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
         {
 
         }
+
         public DbSet<Filme> Filmes { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
     }
 }
