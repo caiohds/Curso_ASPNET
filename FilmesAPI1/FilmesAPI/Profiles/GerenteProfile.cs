b6d410c597532d4cc1e0e0b9de64a9ccdc735977
caiohds <caiohds2020@gmail.com>
2022-10-04 14:51:22 -0300

    Criação do GerenteProfile

--- FilmesAPI1/FilmesAPI/Profiles/GerenteProfile.cs
+++ FilmesAPI1/FilmesAPI/Profiles/GerenteProfile.cs
@@ -1,6 +1,15 @@
-﻿namespace FilmesAPI.Profiles
+﻿using AutoMapper;
+using FilmesAPI.data.dtos.Gerente;
+using FilmesAPI.models;
+
+namespace FilmesAPI.Profiles
 {
-    public class GerenteProfile
+    public class GerenteProfile : Profile
     {
+        public GerenteProfile()
+        {
+            CreateMap<CreateGerenteDto, Gerente>();
+            CreateMap<Gerente, ReadGerenteDto>();
+        }
     }
 }
