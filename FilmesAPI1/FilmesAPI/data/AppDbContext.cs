cf3f6ad610821905ce4e6a2550f28cbe028bc573
caiohds <caiohds2020@gmail.com>
2022-10-04 00:04:25 -0300

    Criação do EnderecoController

--- FilmesAPI1/FilmesAPI/data/AppDbContext.cs
+++ FilmesAPI1/FilmesAPI/data/AppDbContext.cs
@@ -13,5 +13,6 @@ namespace FilmesAPI.data
 
         public DbSet<Filme> Filmes { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
     }
 }
