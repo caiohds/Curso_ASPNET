1f984e150db3c22b0136dc110b294c0caecfb155
caiohds <caiohds2020@gmail.com>
2022-10-04 15:05:54 -0300

    Criação do GerenteController

---
+++ FilmesAPI1/FilmesAPI/Controllers/GerenteController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using FilmesAPI.data;
+using FilmesAPI.data.dtos.Gerente;
+using FilmesAPI.models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GerenteController : ControllerBase
+    {
+        private AppDbContext _context;
+        private IMapper _mapper;
+
+        public GerenteController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        [HttpPost]
+        public IActionResult AdicionaGerente(CreateGerenteDto dto)
+        {
+            Gerente gerente = _mapper.Map<Gerente>(dto);
+            _context.Gerentes.Add(gerente);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(RecuperaGerentesPorId), new { Id = gerente.Id }, gerente);
+        }
+
+        public IActionResult RecuperaGerentesPorId (int id)
+        {
+            Gerente gerente = _context.Gerentes.FirstOrDefault(gerente => Gerente.Id == id);
+            if(gerente != null)
+            {
+                ReadGerenteDto gerenteDto = _mapper.Map<ReadGerenteDto>(gerente);
+
+                return Ok(gerenteDto);
+            }
+            return NotFound();
+        }
+    }
+}
--- FilmesAPI1/FilmesAPI/data/AppDbContext.cs
+++ FilmesAPI1/FilmesAPI/data/AppDbContext.cs
@@ -17,5 +17,7 @@ namespace FilmesAPI.data
         public DbSet<Filme> Filmes { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
+
+        public DbSet<Gerente> Gerentes { get; set; }
     }
 }
