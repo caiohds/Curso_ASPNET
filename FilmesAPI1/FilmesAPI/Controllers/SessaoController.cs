6cc9b35c190103267dd84eba306a4e0ed733c66d
caiohds <caiohds2020@gmail.com>
2022-10-05 16:31:13 -0300

    Criação do SessaoController

---
+++ FilmesAPI1/FilmesAPI/Controllers/SessaoController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using FilmesAPI.data;
+using FilmesAPI.models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SessaoController : ControllerBase
+    {
+        private AppDbContext _context;
+        private IMapper _mapper;
+
+        public SessaoController(AppDbContext context,IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        [HttpPost]
+        public IActionResult AdicionaSessao(CreateSessaoDto dto)
+        {
+            Sessao sessao = _mapper.Map<Sessao>(dto);
+            _context.Sessoes.Add(sessao);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(RecuperaSessaoPorId), new { Id = sessao.Id }, sessao);
+        }
+    }
+}
