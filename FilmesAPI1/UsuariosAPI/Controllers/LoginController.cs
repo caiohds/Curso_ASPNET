96fcb00f75590e30f02efa3b62bbd557d783f7b7
caiohds <caiohds2020@gmail.com>
2022-10-20 14:24:03 -0300

    Criação da classe 'LoginRequest'

---
+++ FilmesAPI1/UsuariosAPI/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UsuariosAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private LoginService _loginService;
+
+        [HttpPost]
+        public IActionResult LogarUsuario(LoginRequest request)
+        {
+
+            return Ok();
+        }
+    }
+}
